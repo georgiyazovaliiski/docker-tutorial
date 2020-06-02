0e770788bd0c47670f589e5eaff61e91a64f3276
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2020-06-02 09:49:17 +0300

    Feature change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit   bka  <code>src/App.js</code> and save to reload.
+          Edit   on FEATURE BRANCH  <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
